2aa58b56b383b8ed761a0c32d0b9bfe8e68dbcee
cosmas28 <augubilla100@gmail.com>
2023-10-06 19:46:48 +0300

    refactor: refine add todo behavior

--- src/components/AddTodo/index.tsx
+++ src/components/AddTodo/index.tsx
@@ -1,13 +1,33 @@
 import React, { FC, useState, FormEvent } from 'react'
-import { useTodo } from '../../context/TodoProvider'
+import { useTodo, BASE_URL } from '../../context/TodoProvider'
 import './index.scss'
 
 const AddTodo: FC = () => {
-	const { addTodoItem } = useTodo()
-	const [title, setTitle] = useState<string | undefined>()
+	const { setAllTodos, todos } = useTodo()
+	const [title, setTitle] = useState<string>('')
+	const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
 
 	const handleTitleChange = (event: FormEvent<HTMLInputElement>) => {
 		setTitle(event.currentTarget.value)
+	}
+
+	const addTodoItem = (title: string) => {
+		setIsSubmitting(true)
+		fetch(`${BASE_URL}/api/todos`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application.json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ title }),
+      cache: 'default'
+    })
+		.then(async(res) => {
+			const jsonRes = await res.json()
+			if (todos) setAllTodos?.([...todos, jsonRes.todo])
+			setIsSubmitting(false)
+			setTitle('')
+		})
 	}
 
 	const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -19,9 +39,9 @@ const AddTodo: FC = () => {
 	return (
 		<form onSubmit={handleOnSubmit} className='add-todo'>
 			<label className='add-todo__input-wrapper' htmlFor='title'>
-				<input width="100%" id='title' name='title' type='text' onChange={handleTitleChange} />
+				<input width="100%" id='title' name='title' type='text' value={title} onChange={handleTitleChange} />
 			</label>
-			<button type='submit' disabled={!title} className='add-todo__submit-button'>Add</button>
+			<button type='submit' disabled={!title} className='add-todo__submit-button'>{isSubmitting ? 'Loading...' : 'Add'}</button>
 		</form>
 	)
 }
--- src/context/TodoProvider/index.tsx
+++ src/context/TodoProvider/index.tsx
@@ -1,13 +1,11 @@
 import React, { useContext, createContext, ReactElement, FC, useState, useEffect} from 'react'
-import { Todo, Action } from '../../types'
+import { Todo } from '../../types'
 
 export const BASE_URL = 'http://localhost:5173'
 
 type Props = {
 	todos: Todo[]
-	dispatch: (action: Action) => void
 	loading: boolean
-	addTodoItem: (title: string) => void
 	setAllTodos: (todos: Todo[]) => void
 }
 
@@ -30,28 +28,11 @@ export const TodoProvider: FC<{children: ReactElement}> = ({children}) => {
     })
 	}, [])
 
-	const addTodoItem = (title: string) => {
-		fetch(`${BASE_URL}/api/todos`, {
-      method: 'POST',
-      headers: {
-        Accept: 'application.json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ title }),
-      cache: 'default'
-    })
-		.then(async(res) => {
-			const jsonRes = await res.json()
-			setAllTodos([...todos, jsonRes.todo])
-		})
-	}
-
 	return (
 		<TodoContext.Provider value={
 			{
 				todos,
 				loading,
-				addTodoItem,
 				setAllTodos
 			}
 		}>
